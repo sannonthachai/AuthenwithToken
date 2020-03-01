bc0a5f93acba7e655b952589f1cb3250b7d3bf12
sannonthachai <sannonthachai@gmail.com>
2020-03-01 13:13:46 +0700

    Update lib

--- config/key.js
+++ config/key.js
@@ -1,5 +1,5 @@
 dbPassword = 'mongodb+srv://sannonthachai:chai41742@cluster0-b4miu.mongodb.net/test?retryWrites=true&w=majority'
-setMongoose = { useCreateIndex: true , useNewUrlParser: true , useFindAndModify: false }
+setMongoose = { useCreateIndex: true , useNewUrlParser: true , useFindAndModify: false, useUnifiedTopology: true }
 secretJWT = 'CRIMSON_SECRET_KEY'
 
 module.exports = {
